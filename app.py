fbcce58894a556760226acb8abb523b2500bb3e2
riyathaslik <riyathasli6954@gmail.com>
2026-04-25 11:47:30 +0530

    req

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# app.py
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -8,4 +8,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
